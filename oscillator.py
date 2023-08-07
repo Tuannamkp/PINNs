dc9021d2b75f6fa7a1d9154c74223a1d8985b5a4
Tuannamkp <106246937+Tuannamkp@users.noreply.github.com>
2023-08-07 22:25:51 +0900

    Add files via upload

---
+++ oscillator.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+def oscillator(d,w0,x):
+    assert d<w0
+    w = np.sqrt(w0**2 - d**2)
+    phi = np.arctan(-d/w)
+    A = 1/(2*np.cos(phi))
+    cos = torch.cos(phi + w*x)
+    sin = torch.sin(phi +w*x)
+    exp = torch.exp(-d*x)
+    y = exp*(2*A*cos)
+    return y
